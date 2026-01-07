efbf204515aa5b37d43ba9876f3156e86b02e102
daniel <hun320daniel07@gmail.com>
2026-01-07 14:51:27 +0900

    채팅관련 패키지 삭제

--- src/main/java/pard/server/com/longkathon/chat/ChatMessageRepo.java
+++
@@ -1,9 +0,0 @@
-package pard.server.com.longkathon.chat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ChatMessageRepo extends JpaRepository<ChatMessage, Long> {
-    Optional<ChatMessage> findByChannelUrlAndMessageId(String channelUrl, Long messageId);
-}
--- src/main/java/pard/server/com/longkathon/chat/WebhookInboxRepo.java
+++
@@ -1,7 +0,0 @@
-package pard.server.com.longkathon.chat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WebhookInboxRepo extends JpaRepository<WebhookInbox, Long> {
-    boolean existsByDedupKey(String dedupKey);
-}
