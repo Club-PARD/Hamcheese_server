487d920da47560268d9a9b45477eac6531b57dcc
daniel <hun320daniel07@gmail.com>
2026-02-04 21:37:23 +0900

    TestCommit

--- src/main/java/pard/server/com/longkathon/LongkathonApplication.java
+++ src/main/java/pard/server/com/longkathon/LongkathonApplication.java
@@ -3,6 +3,7 @@ package pard.server.com.longkathon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//롱커톤 원본 브랜치
 @SpringBootApplication
 public class LongkathonApplication {
 
